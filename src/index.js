f1c6b78eb6b11ab9b519eabae2e843c290997689
Kostiantyn <kosssm155@gmail.com>
2021-09-26 17:37:41 +0300

    redux-middleware

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { applyMiddleware, createStore } from 'redux';
+import { counterReducer } from './reducer';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const myLogger = store => next => action => {
+	return next(action);
+};
+
+const secondMiddleware = store => next => action => {
+	return next(action);
+};
+
+const capAtTen = store => next => action => {
+	if (store.getState() >= 10) {
+		return next({ type: 'DECREMENT' });
+	}
+	next(action);
+};
+
+const store = createStore(
+	counterReducer,
+	applyMiddleware(myLogger, secondMiddleware, capAtTen, logger)
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
